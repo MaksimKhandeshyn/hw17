45d5423d0b8a7131f4ef3df25e003294c1afe1c7
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-02-20 00:19:45 +0200

    start make hw

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".button");
+const input = document.querySelector(".input-text");
+btn.style.text = "This isnt btn";
+// btn.addEventListener("click", function () {
+//   btn.style.text = input.style.text;
+// });
+console.log(btn);
+console.log(input);
