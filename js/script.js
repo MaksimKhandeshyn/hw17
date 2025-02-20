544a5550540f9dcb8285c6acd54c646a430fafe3
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-02-20 22:20:56 +0200

    make almost all hw

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+//--1--
 const btn = document.querySelector(".button");
 const input = document.querySelector(".input-text");
-btn.style.text = "This isnt btn";
-// btn.addEventListener("click", function () {
-//   btn.style.text = input.style.text;
-// });
-console.log(btn);
-console.log(input);
+
+btn.addEventListener("click", function () {
+  btn.innerHTML = input.value;
+});
+
+//--2--
+const img = (document.querySelector(".first-img").src = "../second.jpg");
+
+//--3--
+const link = (document.querySelector(".link__to-img").href =
+  "https://www.linkedin.com/pulse/what-difference-between-picture-photo-photoshopexpert/");
+const img2 = document.querySelector(".second-img");
+console.log(img2);
+const changeImg = (img2.alt = "center which appears plane");
+console.log(changeImg);
