6a1ea3735417ed56d8b9a44a15f279a728a1f7f3
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-02-21 18:49:32 +0200

    make hw

--- js/script.js
+++ js/script.js
@@ -12,7 +12,16 @@ const img = (document.querySelector(".first-img").src = "../second.jpg");
 //--3--
 const link = (document.querySelector(".link__to-img").href =
   "https://www.linkedin.com/pulse/what-difference-between-picture-photo-photoshopexpert/");
+
 const img2 = document.querySelector(".second-img");
 console.log(img2);
+
 const changeImg = (img2.alt = "center which appears plane");
 console.log(changeImg);
+
+//--4--
+const item = document.querySelector(".tasks__list-item");
+console.log(item.parentNode);
+const list = document.querySelector(".tasks-list");
+console.log(list.childNodes[1]);
+list.childNodes[1].innerHTML = "This is new text";
